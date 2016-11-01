48583b09be7d34205e28e8a01c0f5da2a07554f4
Hawk <content921@gmail.com>
2016-11-01 19:17:50 +0500

    Mp3

--- app/src/main/java/com/example/hawk/mp3basic/MainActivity.java
+++ app/src/main/java/com/example/hawk/mp3basic/MainActivity.java
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playBtn = (Button) findViewById(R.id.play_btn);
+        pauseBtn = (Button) findViewById(R.id.pause_btn);
+
+
     }
 }
