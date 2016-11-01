4cce5969f76620512c1d94c2ab4fec70cde1f7df
Hawk <content921@gmail.com>
2016-11-01 19:40:35 +0500

    Mp3 onCompletion Listener

--- app/src/main/java/com/example/hawk/mp3basic/MainActivity.java
+++ app/src/main/java/com/example/hawk/mp3basic/MainActivity.java
@@ -1,19 +1,54 @@
 package com.example.hawk.mp3basic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button playBtn,pauseBtn;
+    private  MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         playBtn = (Button) findViewById(R.id.play_btn);
         pauseBtn = (Button) findViewById(R.id.pause_btn);
 
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.vital);
 
+         playBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 mediaPlayer.start();
+             }
+         });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+            }
+        });
+    }
+
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+                releaseMediaPlayer();
+        }
+    };
+
+    private void releaseMediaPlayer(){
+
+        if(mediaPlayer!=null){
+
+            mediaPlayer.release();
+        }
+
+        mediaPlayer =null;
     }
 }
